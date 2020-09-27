371cdfe7a04ccd11b182fde579ea2bcda8baa1d2
songxiaoying <songxiaoying@tianyancha.com>
2020-09-27 14:22:58 +0800

    add articles get delete

--- index.js
+++ index.js
@@ -4,10 +4,34 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+const articles = [{title: "example"}]
 app.get('/', (req,res) => {
   res.send('hello world')
+})
+
+app.get('/articles', (req,res, next) => {
+  res.send(articles)
+})
+
+app.post('/articles', (req,res, next) => {
+  res.send('ok')
+})
+
+app.get('/articles/:id', (req,res, next) => {
+  const id = req.params.id;
+  console.log('fetcing: ', id);
+  res.send(articles[id])
+});
+
+app.delete('/articles/:id', (req, res, next) => {
+  const id = req.params.id;
+  console.log('deleting: ' + id);
+  delete articles[id];
+  res.send({message: 'delete'});
 })
 
 app.listen(port, () => {
   console.log(`express web app available at localhost: ${port}`);
 })
+
+module.exports = app;
